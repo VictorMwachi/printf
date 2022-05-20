7124317ae271871ffad6a6479c63031e9969954e
root <mudakizevictor@gmail.com>
2022-05-20 06:23:10 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -5,3 +5,29 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list arguements;
+	unsigned int i;
+	char *str;
+	char *s;
+
+	va_start(arguements,format);
+	for (str = format; str !='\0'; str++)
+	{
+		while (*str != '%')
+		{
+			putchar(*str);
+			*str++;
+		}
+		*str++;
+		switch(*str)
+		{
+			case 'c': i = va_arg(arguements,int);
+				  putchar(i);
+				  break;
+			case 's': i = va_arg(arguements,char *);
+				  puts(s);
+				  break;
+		}
+	}
+	va_end(arguements);
+}
