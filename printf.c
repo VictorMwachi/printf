52e4242237b25b3b0e85313d3b32513b7bdd2484
root <mudakizevictor@gmail.com>
2022-05-20 04:52:11 -0700

    main.h

---
+++ printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _printf - produces output to stdio according to format
+ * @format: string containing characters and specifiers
+ */
+int _printf(const char *format, ...)
+{
