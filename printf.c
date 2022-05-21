dae1a8be08bc53c31e4dc36f4bcbdde4e50c0d76
victormwachi <mudakizevictor@gmail.com>
2022-05-21 21:54:57 +0300

    update

--- printf.c
+++ printf.c
@@ -5,7 +5,6 @@ int _printf(const char* format,...)
     unsigned int i;
     char *s;
 
-    //Module 1: Initializing Myprintf's arguments
     va_list arg;
     va_start(arg, format);
 
@@ -15,43 +14,28 @@ int _printf(const char* format,...)
         while( *traverse != '%' )
         {
             if(*traverse=='\0')
-        {return;}
+	    {
+		    return;
+	    }
             putchar(*traverse);
             traverse++;
         }
 
         traverse++;
 
-        //Module 2: Fetching and executing arguments
         switch(*traverse)
         {
             case 'c' : i = va_arg(arg,int);     //Fetch char argument
                         putchar(i);
                         break;
 
-           /** case 'd' : i = va_arg(arg,int);         //Fetch Decimal/Integer argument
-                        if(i<0)
-                        {
-                            i = -i;
-                            putchar('-');
-                        }
-                        puts(convert(i,10));
-                        break;
-
-            case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
-                        puts(convert(i,8));
-                        break;*/
 
             case 's': s = va_arg(arg,char *);       //Fetch string
                         puts(s);
                         break;
 
-            /**case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
-                        puts(convert(i,16));
-                        break; */
         }
     }
 
-    //Module 3: Closing argument list to necessary clean-up
     va_end(arg);
 }
