76e6889a10f4bed43bffd855924b24861fc5bbcc
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-05-21 23:58:28 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stdlib.h>
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 int _printf(const char *format, ...);
 int _putchar(char c);
 #endif
